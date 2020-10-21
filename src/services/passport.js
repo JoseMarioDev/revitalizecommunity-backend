f8ecab3361cfebc84453f52562b26a8f580a4e94
josemariodev <josemariodev@gmail.com>
2020-10-21 09:38:19 -0500

    heroku cli init

--- src/services/passport.js
+++ src/services/passport.js
@@ -1,7 +1,7 @@
 import { prisma } from '../generated/prisma-client';
 import passport from 'passport';
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FacebookStrategy = require('passport-facebook').Strategy;
+// const FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.serializeUser((user, done) => {
 	done(null, user.id);
@@ -62,47 +62,47 @@ passport.use(
 	)
 );
 
-passport.use(
-	new FacebookStrategy(
-		{
-			clientID: process.env.FACEBOOK_APP_ID,
-			clientSecret: process.env.FACEBOOK_APP_SECRET,
-			callbackURL: '/auth/facebook/callback',
-			proxy: true
-		},
-		async (accessToken, refreshToken, profile, done) => {
-			try {
-				const existingUser = await prisma.query.userProfiles({
-					where: {
-						userAccountId: profile.id
-					}
-				});
+// passport.use(
+// 	new FacebookStrategy(
+// 		{
+// 			clientID: process.env.FACEBOOK_APP_ID,
+// 			clientSecret: process.env.FACEBOOK_APP_SECRET,
+// 			callbackURL: '/auth/facebook/callback',
+// 			proxy: true
+// 		},
+// 		async (accessToken, refreshToken, profile, done) => {
+// 			try {
+// 				const existingUser = await prisma.query.userProfiles({
+// 					where: {
+// 						userAccountId: profile.id
+// 					}
+// 				});
 
-				if (existingUser.length) {
-					// we already have a row with the given googleId
-					return done(null, existingUser[0]);
-				}
+// 				if (existingUser.length) {
+// 					// we already have a row with the given googleId
+// 					return done(null, existingUser[0]);
+// 				}
 
-				// Create new account if user does not exists
-				await prisma.mutation.createExternalAccount({
-					data: {
-						accountId: profile.id
-					}
-				});
+// 				// Create new account if user does not exists
+// 				await prisma.mutation.createExternalAccount({
+// 					data: {
+// 						accountId: profile.id
+// 					}
+// 				});
 
-				const userProfile = await prisma.mutation.createUserProfile({
-					data: {
-						email: profile.emails[0].value,
-						userAccountId: profile.id,
-						firstName: profile.name.givenName !== undefined ? profile.name.givenName : null,
-						lastName: profile.name.familyName !== undefined ? profile.name.familyName : null
-					}
-				});
+// 				const userProfile = await prisma.mutation.createUserProfile({
+// 					data: {
+// 						email: profile.emails[0].value,
+// 						userAccountId: profile.id,
+// 						firstName: profile.name.givenName !== undefined ? profile.name.givenName : null,
+// 						lastName: profile.name.familyName !== undefined ? profile.name.familyName : null
+// 					}
+// 				});
 
-				done(null, userProfile);
-			} catch (err) {
-				throw new Error(err);
-			}
-		}
-	)
-);
+// 				done(null, userProfile);
+// 			} catch (err) {
+// 				throw new Error(err);
+// 			}
+// 		}
+// 	)
+// );
