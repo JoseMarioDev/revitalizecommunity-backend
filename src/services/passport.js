61721fed6231a2fce4e7834a3ef23c1b80f491c8
josemariodev <josemariodev@gmail.com>
2020-10-21 14:32:13 -0500

    comment out tw strategy

--- src/services/passport.js
+++ src/services/passport.js
@@ -2,8 +2,8 @@ import passport from 'passport';
 
 import { OAuthGoogle } from './oauth/google';
 // import { OAuthFacebook } from './oauth/facebook';
-import { OAuthTwitter } from './oauth/twitter';
+// import { OAuthTwitter } from './oauth/twitter';
 
 passport.use(OAuthGoogle);
 // passport.use(OAuthFacebook);
-passport.use(OAuthTwitter);
+// passport.use(OAuthTwitter);
