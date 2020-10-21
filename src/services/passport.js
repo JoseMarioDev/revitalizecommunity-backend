afe06b2b1bf4e029db2a8087989ca40a1ce27731
josemariodev <josemariodev@gmail.com>
2020-10-21 14:26:39 -0500

    comment out fb strategy

--- src/services/passport.js
+++ src/services/passport.js
@@ -1,9 +1,9 @@
 import passport from 'passport';
 
 import { OAuthGoogle } from './oauth/google';
-import { OAuthFacebook } from './oauth/facebook';
+// import { OAuthFacebook } from './oauth/facebook';
 import { OAuthTwitter } from './oauth/twitter';
 
 passport.use(OAuthGoogle);
-passport.use(OAuthFacebook);
+// passport.use(OAuthFacebook);
 passport.use(OAuthTwitter);
